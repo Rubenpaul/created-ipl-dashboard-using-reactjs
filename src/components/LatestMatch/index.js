7bd7d95a1d988bb87fd015d622835cb3df104505
Rubenpaul <94739637+Rubenpaul@users.noreply.github.com>
2022-12-19 14:03:16 +0530

    updated changes

--- src/components/LatestMatch/index.js
+++ src/components/LatestMatch/index.js
@@ -1,1 +1,21 @@
 // Write your code here
+import './index.css'
+
+import {Component} from 'react'
+
+class LatestMatch extends Component {
+  render() {
+    const {latestMatchDetails} = this.props
+    console.log(latestMatchDetails)
+    console.log(latestMatchDetails.competingTeam)
+    return (
+      <div>
+        <p className="latest-matches-text">LatestMatches</p>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+export default LatestMatch
+
