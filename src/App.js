b7aa96d0710ed6bf970829df0f80722028fd1e22
Rubenpaul <94739637+Rubenpaul@users.noreply.github.com>
2022-12-21 13:51:17 +0530

    updated changes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {Switch, Route} from 'react-router-dom'
 
 import Home from './components/Home'
 import TeamMatches from './components/TeamMatches'
+import NotFound from './components/NotFound'
 
 const App = () => (
   <div className="app-container">
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/team-matches/:id" component={TeamMatches} />
+      <Route component={NotFound} />
     </Switch>
   </div>
 )
